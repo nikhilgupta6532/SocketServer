36510895fbc65c6d7dc8521057ff314e43a13280
Nikhil <nikhilgupta6532@gmail.com>
2018-02-03 16:43:17 +0530

    dfda

--- server.js
+++ server.js
@@ -7,58 +7,51 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 const PORT = process.env.PORT || 8080;
-//
-// io.on('connection', socket => {
-//   console.log('new user connected');
-//   socket.on('newnote', message => {
-//     console.log(message.note);
-//     socket.emit('sendNote', {
-//       notes: message.note
-//     });
-//   });
-//   socket.on('disconnect', () => {
-//     console.log('user was disconnected');
-//   });
-// });
 
-io.of('/backend').on('connection', socket => {
-  socket.on('message', (message, callback) => {
-    console.log(message);
-    io
-      .of('/backend')
-      .to(rooms.name)
-      .emit('sendNote', {
-        message: message.message
-      });
-    callback();
-  });
-  socket.on('join', (params, callback) => {
-    socket.join(params.name);
-    rooms.name = params.name;
-    console.log(socket.rooms);
-    // socket.on('message', message => {
-    //   console.log(message.message);
-    //   io
-    //     .of('/backend')
-    //     .to(params.name)
-    //     .emit('sendNote', {
-    //       message: message.message
-    //     });
-    // });
-    callback();
-  });
-  socket.on('chat', socket_id => {
-    io
-      .of('/backend')
-      .to(socket_id.id)
-      .emit('sendNote', {
-        message: socket_id.message
-      });
+io.on('connection', socket => {
+  console.log('new user connected');
+  socket.on('newnote', message => {
+    console.log(message.note);
+    socket.emit('sendNote', {
+      notes: message.note
+    });
   });
   socket.on('disconnect', () => {
     console.log('user was disconnected');
   });
 });
+//
+// io.on('connection', socket => {
+//   socket.on('message', (message, callback) => {
+//     io.to(rooms.name).emit('sendNote', {
+//       message: message.message
+//     });
+//     callback();
+//   });
+//   socket.on('join', (params, callback) => {
+//     socket.join(params.name);
+//     rooms.name = params.name;
+//     console.log(socket.rooms);
+//     // socket.on('message', message => {
+//     //   console.log(message.message);
+//     //   io
+//     //     .of('/backend')
+//     //     .to(params.name)
+//     //     .emit('sendNote', {
+//     //       message: message.message
+//     //     });
+//     // });
+//     callback();
+//   });
+//   socket.on('chat', socket_id => {
+//     io.to(socket_id.id).emit('sendNote', {
+//       message: socket_id.message
+//     });
+//   });
+//   // socket.on('disconnect', () => {
+//   //   console.log('user was disconnected');
+//   // });
+// });
 
 server.listen(PORT, () => {
   console.log(`server is up on port ${PORT}`);
